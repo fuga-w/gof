92a66234fb0ce61609ee994c12ace51bf2d4771c
fuga-w <f-watanabe@kk-tsc.com>
2021-08-13 09:14:35 +0900

    変更追加

---
+++ lib/command/command.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/cupertino.dart';
+
+abstract class Command {
+  execute() {}
+}
+
+class MacroCommand extends Command {
+  List<Command> _commands = [];
+  List<Command> _removed = [];
+
+  append(Command command) {
+    _commands.add(command);
+  }
+
+  clear() {
+    _commands.clear();
+  }
+
+  @override
+  execute() {
+    for (var command in _commands) {
+      _removed.clear();
+      command.execute();
+    }
+  }
+
+  undo() {
+    _removed.add(
+      _commands.removeLast(),
+    );
+  }
+
+  redo() {
+    _commands.add(
+      _removed.removeLast(),
+    );
+  }
+}
+
+class PaintCommand extends Command {
+  final Offset _point;
+
+  PaintCommand
+}
